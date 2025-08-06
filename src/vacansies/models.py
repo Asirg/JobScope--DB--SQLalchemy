36f7132a7977c08ad4c38b31849f8e2c223d9f8d
Asirg <52125449+Asirg@users.noreply.github.com>
2025-08-06 11:34:31 +0300

    update models and set postgis

--- src/vacansies/models.py
+++ src/vacansies/models.py
@@ -1,13 +1,18 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import ForeignKey
 
+from geoalchemy2 import Geography
+
 from typing import Optional
 
 import datetime
+import enum
+
 
 from src.annotated import intpk, created_at, updated_at, name255
 
 from src.database import Base
+
 
 
 class Sources(Base):
@@ -71,7 +76,22 @@ class LanguageLevels(Base):
     __tablename__ = "language_levels"
 
     language_level_id: Mapped[intpk]
+    language_id: Mapped[int] = mapped_column(ForeignKey("languages.language_id", ondelete="CASCADE"))
     name: Mapped[name255]
+
+class Locations(Base):
+    __tablename__ = "locations"
+    location_id: Mapped[intpk]
+    name:Mapped[str]
+    location_point:Mapped[Geography] = mapped_column(Geography(geometry_type="POINT", srid=4326))
+
+class Workload(enum.Enum):
+    parttime = "parttime"
+    fulltime = "fulltime"
+
+class WorkLocationType(enum.Enum):
+    remote = "remote"
+    office = "office"
 
 ########################################################
 
@@ -79,14 +99,25 @@ class Vacancies(Base):
     __tablename__ = "vacancies"
 
     vacancy_id: Mapped[intpk]
-
     raw_vacancy_id: Mapped[int] = mapped_column(ForeignKey("raw_vacancies.raw_vacancy_id", ondelete="SET NULL"))
 
     name: Mapped[name255]
-    experience: Mapped[Optional[int]]
+    company_id: Mapped[int] = mapped_column(ForeignKey("companies.company_id", ondelete="SET NULL"))
+    location_id: Mapped[int | None] = mapped_column(ForeignKey("locations.location_id", ondelete="SET NULL"))
 
+    experience: Mapped[Optional[int]]
+    main_language_id: Mapped[int] = mapped_column(ForeignKey("languages.language_id", ondelete="SET NULL"))
+    compensation_min: Mapped[int | None]
+    compensation_max: Mapped[int | None]
     company_id: Mapped[int] = mapped_column(ForeignKey("companies.company_id", ondelete="SET NULL"))
     speciality_id: Mapped[int] = mapped_column(ForeignKey("specialities.speciality_id", ondelete="SET NULL"))
+
+    workload: Mapped[Workload] = mapped_column(server_default="fulltime")
+    work_location_type: Mapped[WorkLocationType] = mapped_column(server_default="office")
+
+
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
 
 
 class VacanciesExperienceSpecialities(Base):
@@ -112,5 +143,18 @@ class VacanciesTechnologies(Base):
 
     technology_id: Mapped[int] = mapped_column(
         ForeignKey("technologies.technology_id", ondelete="CASCADE")
+        , primary_key=True
+    )
+
+class VacanciesAdditionalLanguages(Base):
+    __tablename__ = 'vacancies_additional_languages'
+
+    vacancy_id: Mapped[int] = mapped_column(
+        ForeignKey("vacancies.vacancy_id", ondelete="CASCADE")
+        , primary_key=True
+    )
+
+    language_level_id: Mapped[int] = mapped_column(
+        ForeignKey("language_levels.language_level_id", ondelete="CASCADE")
         , primary_key=True
     )
